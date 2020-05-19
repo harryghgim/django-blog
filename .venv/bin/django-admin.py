ee8123e4253e97e8b03e21ceb442b7a371fbbae5
Gwanghyeon Gim <harryghgim@gmail.com>
2020-05-19 16:48:13 +0900

    First Commit

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gwanghyeongim/Documents/py/coreyMS_pj/django_project/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
