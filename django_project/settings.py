fbea9b9f3d9ca050710d238414148fd64f0bbb09
Gwanghyeon Gim <harryghgim@gmail.com>
2020-05-19 17:50:55 +0900

    another commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 from dotenv import load_dotenv
+import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
