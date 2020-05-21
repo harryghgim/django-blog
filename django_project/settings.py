8c46b85d020cf6038f59df14afeecd96d1f0cf42
Gwanghyeon Gim <harryghgim@gmail.com>
2020-05-21 11:58:57 +0900

    allowed host: '127.0.0.1'

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['my-sweet-django-web.herokuapp.com']
+ALLOWED_HOSTS = ['my-sweet-django-web.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
