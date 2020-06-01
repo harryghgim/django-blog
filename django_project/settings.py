08ac6e973923a6f325d0c20f62ea34e43000e8b7
Gwanghyeon Gim <harryghgim@gmail.com>
2020-06-01 23:50:28 +0900

    DNS setting, DEBUG=True, Allowed host edited

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['my-sweet-django-web.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['myawsdjapp.com', 'my-sweet-django-web.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
