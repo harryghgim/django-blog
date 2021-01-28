d338e59b4e6a0a67dac5c6a804a1ecf3b8f714c3
harryghgim <harryghgim@gmail.com>
2021-01-29 01:10:15 +0900

    added comment about prod_settings in manage.py - 히로쿠 배포랑 로컬 서버를 구분하기

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.local_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.local_settings') # in heroku config DJANGO_SETTINGS_MODULE=project.prod_settings
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
